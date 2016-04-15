de42ea2e319a0dc125d48c9a9d28301f08e02341
Yousef Chanan <yousef@burza.hr>
2016-04-15 13:32:21 +0200

    feature(upgrade): Upgrade for Sylius 0.17

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace Webburza\Sylius\ArticleBundle\Controller;
 
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Webburza\Sylius\ArticleBundle\Doctrine\ORM\ArticleRepository;
 
@@ -10,45 +12,55 @@ class ArticleController extends ResourceController
     /**
      * Show publicly visible articles for the current locale.
      *
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexFrontAction()
+    public function indexFrontAction(Request $request)
     {
         // Get current locale
         $locale = $this->get('sylius.context.locale')->getCurrentLocale();
 
+        // Get request configuration
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
         /** @var ArticleRepository $repository */
-        $repository = $this->getRepository();
+        $repository = $this->repository;
 
         // Get a paginator for publicly visible articles for locale
         $articlesPaginator = $repository->getPublicPaginatorForLocale($locale);
         $articlesPaginator->setCurrentPage($this->get('request')->get('page', 1), true, true);
-        $articlesPaginator->setMaxPerPage($this->config->getPaginationMaxPerPage());
+        $articlesPaginator->setMaxPerPage($configuration->getPaginationMaxPerPage());
 
-        $view = $this
-            ->view()
-            ->setTemplate('WebburzaSyliusArticleBundle:Frontend:index.html.twig')
-            ->setData(array(
-                'articles' => $articlesPaginator
-            ))
-        ;
+        // Create the view
+        $view = View::create();
 
-        return $this->handleView($view);
+        // Set template and data
+        $view->setTemplate('WebburzaSyliusArticleBundle:Frontend:index.html.twig');
+        $view->setData(array(
+            'articles' => $articlesPaginator
+        ));
+
+        // Handle view
+        return $this->viewHandler->handle($configuration, $view);
     }
 
     /**
      * Get a publicly visible article by slug, for the current locale.
      *
+     * @param Request $request
      * @param $slug
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function showFrontAction($slug)
+    public function showFrontAction(Request $request, $slug)
     {
         // Get current locale
         $locale = $this->get('sylius.context.locale')->getCurrentLocale();
 
+        // Get request configuration
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
         /** @var ArticleRepository $repository */
-        $repository = $this->getRepository();
+        $repository = $this->repository;
 
         // Get a publicly visible article by translated slug
         $article = $repository->findPublicBySlug($slug, $locale);
@@ -57,14 +69,16 @@ class ArticleController extends ResourceController
             throw $this->createNotFoundException();
         }
 
-        $view = $this
-            ->view()
-            ->setTemplate('WebburzaSyliusArticleBundle:Frontend:show.html.twig')
-            ->setData(array(
-                'article' => $article
-            ))
-        ;
+        // Create the view
+        $view = View::create();
 
-        return $this->handleView($view);
+        // Set template and data
+        $view->setTemplate('WebburzaSyliusArticleBundle:Frontend:show.html.twig');
+        $view->setData(array(
+            'article' => $article
+        ));
+
+        // Handle view
+        return $this->viewHandler->handle($configuration, $view);
     }
 }
--- EventListener/PublishedListener.php
+++ EventListener/PublishedListener.php
@@ -2,13 +2,21 @@
 namespace Webburza\Sylius\ArticleBundle\EventListener;
 
 use DateTime;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Sylius\Component\Resource\Event\ResourceEvent;
 use Sylius\Component\Resource\Exception\UnexpectedTypeException;
 use Webburza\Sylius\ArticleBundle\Entity\Article;
 
 class PublishedListener
 {
-    public function setPublishedAt(ResourceEvent $event)
+    /**
+     * If the article is published, and has no publish date,
+     * set it the the current datetime.
+     *
+     * @param ResourceControllerEvent $event
+     * @throws UnexpectedTypeException
+     */
+    public function setPublishedAt(ResourceControllerEvent $event)
     {
         /** @var Article $subject */
         $subject = $event->getSubject();
